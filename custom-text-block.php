09854415d0e660af88464f9dae6787d3f17e6b4d
BjorksundDesign <thomas@bjorksund.design>
2025-11-13 12:08:29 +0100

    Trying to trigger gitUpdater

--- custom-text-block.php
+++ custom-text-block.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Fastum Block
  * Plugin URI:        https://github.com/BjorksundDesign/wp-blocks-fastum
  * Description:       Block for Fastum public web.
- * Version:           1.01a
+ * Version:           1.01b
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            Thomas Björksund
