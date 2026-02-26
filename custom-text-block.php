bce8d0fc8b4c7b6557d3e9e776b18b8d115e8a40
Thomas <thomas@bjorksund.design>
2026-02-26 16:28:32 +0100

    Release: Bump version

--- custom-text-block.php
+++ custom-text-block.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Fastum Block
  * Plugin URI:        https://github.com/BjorksundDesign/fastum-wordpress-block
  * Description:       Block for Fastum public web.
- * Version:           1.3.29
+ * Version:           1.4.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            Thomas Björksund
